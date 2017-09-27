82c44f4c9b8872fa50762b9763691ff72a3973b3
lol155 <1055455968@qq.com>
2017-09-28 00:06:15 +0800

    jdk 代理简单示例 getProxy

--- src/main/java/JDKProxy/MyInvocationHandler.java
+++ src/main/java/JDKProxy/MyInvocationHandler.java
@@ -2,6 +2,7 @@ package JDKProxy;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 /**
  * Created by wxb on 2017/9/27.
@@ -12,6 +13,7 @@ public class MyInvocationHandler implements InvocationHandler {
 
     /**
      * 构造方法
+     *
      * @param target 目标对象
      */
     public MyInvocationHandler(Object target) {
@@ -23,5 +25,14 @@ public class MyInvocationHandler implements InvocationHandler {
      */
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         return null;
+    }
+
+    /**
+     * 获取目标对象的代理对象
+     * @return 代理对象
+     */
+    public Object getProxy() {
+        return Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(),
+                target.getClass().getInterfaces(), this);
     }
 }
