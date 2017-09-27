fc55ab5f62e03d1edff4e2679d767303e8940f43
lol155 <1055455968@qq.com>
2017-09-27 23:55:40 +0800

    jdk 代理简单示例 创建实现类 实现接口方法

---
+++ src/main/java/JDKProxy/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package JDKProxy;
+
+/**
+ * Created by wxb on 2017/9/27.
+ */
+public class UserServiceImpl implements UserService {
+
+    public void add() {
+        System.out.println("---add --");
+    }
+}
