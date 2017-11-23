960084bd6cab951673a7c08eec027bc08ac043e4
lol155 <1055455968@qq.com>
2017-11-24 00:15:12 +0800

    zookeeper 简单示例 持续监听

--- zookeeper-study/src/main/java/cn/signalfire/bigdata/zk/SimpleDemo.java
+++ zookeeper-study/src/main/java/cn/signalfire/bigdata/zk/SimpleDemo.java
@@ -22,6 +22,11 @@ public class SimpleDemo {
         zkClient = new ZooKeeper("zk1:2181,zk2:2181,zk3:2181", 2000, new Watcher() {
             public void process(WatchedEvent watchedEvent) {
                 System.out.println(watchedEvent.getType());
+                try {
+                    zkClient.getChildren("/", true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
@@ -29,7 +34,7 @@ public class SimpleDemo {
     @Test
     public void testCreatNode() throws Exception {
         // 参数1：要创建的节点的路径 参数2：节点大数据 参数3：节点的权限 参数4：节点的类型
-        String zNode = zkClient.create("/uu", "aaa".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        String zNode = zkClient.create("/uu12", "aaa".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
         System.out.println(zNode);
     }
 
@@ -45,5 +50,6 @@ public class SimpleDemo {
         for (String child : children) {
             System.out.println(child);
         }
+        Thread.sleep(Long.MAX_VALUE);
     }
 }
