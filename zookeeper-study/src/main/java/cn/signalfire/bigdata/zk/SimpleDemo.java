cf423bba33575b099a5a93ef87c0af04fcbc791e
lol155 <1055455968@qq.com>
2017-11-24 00:10:46 +0800

    zookeeper 简单示例 判断是否存在，查看子节点

--- zookeeper-study/src/main/java/cn/signalfire/bigdata/zk/SimpleDemo.java
+++ zookeeper-study/src/main/java/cn/signalfire/bigdata/zk/SimpleDemo.java
@@ -1,12 +1,14 @@
 package cn.signalfire.bigdata.zk;
 
 import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.List;
 
 import static org.apache.zookeeper.ZooDefs.*;
 
@@ -25,9 +27,23 @@ public class SimpleDemo {
     }
 
     @Test
-    public void creatNode() throws Exception {
+    public void testCreatNode() throws Exception {
         // 参数1：要创建的节点的路径 参数2：节点大数据 参数3：节点的权限 参数4：节点的类型
         String zNode = zkClient.create("/uu", "aaa".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
         System.out.println(zNode);
+    }
+
+    @Test
+    public void testExist() throws KeeperException, InterruptedException {
+        Stat stat = zkClient.exists("/uu", false);
+        System.out.println(stat==null?"not exist":"exist");
+    }
+
+    @Test
+    public void testGetChildren() throws KeeperException, InterruptedException {
+        List<String> children = zkClient.getChildren("/", true);
+        for (String child : children) {
+            System.out.println(child);
+        }
     }
 }
