e5dfb1b7ae587824b00246cdb703e5318b770415
lol155 <1055455968@qq.com>
2017-11-26 01:18:07 +0800

    zookeeper 分布式应用系统服务器上下线动态感知程序小练习 server逻辑

---
+++ zookeeper-study/src/main/java/cn/signalfire/bigdata/zk/dist/DistributedServer.java
@@ -0,0 +1,21 @@
+package cn.signalfire.bigdata.zk.dist;
+
+import org.apache.zookeeper.ZooKeeper;
+
+public class DistributedServer {
+
+    private static final String connectString = "zk1:2181,zk2:2181,zk2:2181";
+    private static final int sessionTimeout = 2000;
+    private static final String parentNode = "/servers/";
+
+    private ZooKeeper zooKeeper = null;
+
+
+    public static void main(String[] args) {
+        //获取zk链接
+
+        //利用zk链接注册服务器信息
+
+        //启动业务功能
+    }
+}
